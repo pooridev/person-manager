46783c65b6d5a4f0e53abb4a98b87911bc1884b1
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2020-11-02 19:31:49 +0330

    work with state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Person from './components/Person';
+
 import './App.css';
 
-const App = () => {
-	return (
-		<div className='App'>
-			<h2>Hello World</h2>
-			<hr />
-			<Person firstName='Pooria' lastName='Faramarzian' />
-		</div>
-	);
-};
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			persons : [
+				{firstName :'Pooria' , lastName:'Faramarzian'},
+				{firstName :'Ali' , lastName:'Alavi'},
+				{firstName :'younes' , lastName:'Ebadi'}
+			]
+		}
+	}
+	
+	render() {
+		const {persons} = this.state
+		return (
+			<div className='App'>
+				<h2>Hello World</h2>
+				<hr />
+				<Person firstName={persons[0].firstName} lastName={persons[0].lastName} />
+			</div>
+		);
+	}
+}
 export default App;

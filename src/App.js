09532c519ce45825010e012991b8b03ee2f65ea3
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2020-11-02 19:11:51 +0330

    convert class component to functional component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Person from './components/Person';
 import './App.css';
 
-class App extends Component {
-	render() {
-		return (
-			<div className='App'>
-				<h2>Hello World</h2>
-				<hr />
-				<Person firstName='Pooria' lastName='Faramarzian' />
-			</div>
-		);
-	}
-}
+const App = () => {
+	return (
+		<div className='App'>
+			<h2>Hello World</h2>
+			<hr />
+			<Person firstName='Pooria' lastName='Faramarzian' />
+		</div>
+	);
+};
 export default App;

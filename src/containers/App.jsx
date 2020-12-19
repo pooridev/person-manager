f558c08edf8100e399868e5f9a11d627a1682b21
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2020-12-19 18:28:12 +0330

    custom scrollbar, just like Mac OS

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,83 +2,99 @@ import React, { Component } from 'react';
 
 import Classes from './App.module.css';
 import Persons from './../components/Persons/Persons';
+
 import CockPit from '../components/Cockpit/Cockpit';
 
+import SimpleBar from 'simplebar-react';
+import 'simplebar/dist/simplebar.min.css';
 class App extends Component {
-  constructor(props) {
-    super(props);
-    console.log('[App.js] constructor');
-  };
+	constructor(props) {
+		super(props);
+		console.log('[App.js] constructor');
+	}
 
-  state = {
-    persons:[
-      { name:'Poori' , age: 18, id:'kjhdktt' },
-      { name: 'Ali' , age: 15, id:'lkjdsbf' },
-      { name: 'BlueFish' , age: 20, id:'dddf' }
-    ],
-    shouldShow: true
-  };
-  static getDerivedStateFromProps(props, state) {
-    console.log('[App.js] getDerivedStateFromProps', props);
-    return state;
-  }
-  
-  componentDidMount(){
-    console.log('[App.js] componentDidMount');
-  }
+	state = {
+		persons: [
+			{ name: 'Poori', age: 18, id: 'kjhdktt' },
+			{ name: 'Ali', age: 15, id: 'lkjdsbf' },
+			{ name: 'Eden', age: 34, id: 'httytt' },
+			{ name: 'Hossein', age: 15, id: '454yty' },
+			{ name: 'Soqra', age: 15, id: 'oioiojy' },
+			{ name: 'BlueFish', age: 20, id: 'dddf' }
+		],
+		shouldShow: true
+	};
 
-  handleShowPersons = () => {
-    this.setState({ shouldShow : !this.state.shouldShow });
-  };
+	static getDerivedStateFromProps(props, state) {
+		console.log('[App.js] getDerivedStateFromProps', props);
+		return state;
+	}
 
-  changePersonHandler = (event, personId ) =>{
-    const personIndex = this.state.persons.findIndex(person => person.id === personId);
-    
-    const person = {
-      ...this.state.persons[personIndex]
-    };
-    person.name = event.target.value;
+	componentDidMount() {
+		console.log('[App.js] componentDidMount');
+	}
 
-    const persons = [...this.state.persons];
-    persons[personIndex] = person;
+	handleShowPersons = () => {
+		this.setState({ shouldShow: !this.state.shouldShow });
+	};
 
-    this.setState({ persons });
-  }
+	changePersonHandler = (event, personId) => {
+		const personIndex = this.state.persons.findIndex(
+			person => person.id === personId
+		);
 
-  deletePersonHandler = (personIndex ) => {
-    const persons = [...this.state.persons];
-    persons.splice(personIndex, 1);
-    this.setState({ persons });
-  };
+		const person = {
+			...this.state.persons[personIndex]
+		};
+		person.name = event.target.value;
 
-  render() {
-    console.log('[App.js] render');
+		const persons = [...this.state.persons];
+		persons[personIndex] = person;
 
-    const { shouldShow } = this.state;
-    let persons = null;
-    if(shouldShow){
-      persons = (
-        <div>
-          <Persons
-            persons={this.state.persons} 
-            clicked={this.deletePersonHandler} 
-            changed={this.changePersonHandler} 
-          />
-        </div>
-      )
-    };
+		this.setState({ persons });
+	};
 
-    return (
-      <div className={Classes.App}>
-        <CockPit
-          title={this.props.appTitle}
-          persons={this.state.persons} 
-          clicked={this.handleShowPersons} 
-          showPersons={this.state.shouldShow} />
-        {persons}
-      </div>
-    );
-  };
-};
+	deletePersonHandler = personIndex => {
+		const persons = [...this.state.persons];
+		persons.splice(personIndex, 1);
+		this.setState({ persons });
+	};
+
+	render() {
+		console.log('[App.js] render');
+		const { shouldShow } = this.state;
+		let persons = null;
+		if (shouldShow) {
+			persons = (
+				<div>
+					<Persons
+						persons={this.state.persons}
+						clicked={this.deletePersonHandler}
+						changed={this.changePersonHandler}
+					/>
+				</div>
+			);
+		}
+
+		return (
+			<SimpleBar
+				style={{
+					maxHeight: '100vh',
+					textAlign: 'center'
+				}}
+				autoHide={false}>
+				<div className={Classes.App}>
+					<CockPit
+						title={this.props.appTitle}
+						persons={this.state.persons}
+						clicked={this.handleShowPersons}
+						showPersons={this.state.shouldShow}
+					/>
+					{persons}
+				</div>
+			</SimpleBar>
+		);
+	}
+}
 
 export default App;

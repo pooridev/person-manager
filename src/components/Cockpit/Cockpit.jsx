899fb775c1e1405aaf0dd45368d906bc5b1a2a4b
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2020-12-21 16:05:28 +0330

    hello to useEffect ^_^

--- src/components/Cockpit/Cockpit.jsx
+++ src/components/Cockpit/Cockpit.jsx
@@ -1,29 +1,31 @@
-import React from 'react';
-import classes from './CockPit.module.css'
+import React, { useEffect } from 'react';
+import classes from './CockPit.module.css';
 
-const cockPit = ({ persons, clicked, showPersons, title }) => {
-  const assignedClasses = [];
-  let btnClasses = '';
-  if (showPersons) btnClasses = classes.red;
+const CockPit = ({ persons, clicked, showPersons, title }) => {
+	useEffect(() => {
+		console.log('[Cockpit.jsx] useEffect');
+		// we can send Http request
+		setTimeout(() => {
+			prompt('SALAM');
+		}, 1000);
+	}, [persons]);
 
-  persons.length <= 2 
-    && assignedClasses.push( classes.red );
-  persons.length <= 1 
-    && assignedClasses.push( classes.bold );
-  
-  return(
-    <div className={classes.Cockpit}>
-      <h1>{title}</h1>
-      <p className={assignedClasses.join(' ')}>OMG, It's working</p>
-      <button 
-        onClick={clicked}
-        alt={showPersons}
-        className={btnClasses}>
+	const assignedClasses = [];
+	let btnClasses = '';
+	if (showPersons) btnClasses = classes.red;
 
-        Toggle
-      </button>
-    </div>
-  );
+	persons.length <= 2 && assignedClasses.push(classes.red);
+	persons.length <= 1 && assignedClasses.push(classes.bold);
+
+	return (
+		<div className={classes.Cockpit}>
+			<h1>{title}</h1>
+			<p className={assignedClasses.join(' ')}>OMG, It's working</p>
+			<button onClick={clicked} alt={showPersons} className={btnClasses}>
+				Toggle
+			</button>
+		</div>
+	);
 };
 
-export default cockPit;
+export default CockPit;
